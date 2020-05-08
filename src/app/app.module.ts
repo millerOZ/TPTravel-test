50b05604f00be8c48a40d122e6f7f3101851e21c
Miller <miller.ossa@frow.com.co>
2020-05-07 21:51:52 -0500

    avance redux

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-
+import { appReducers } from './core/store/app.reducers';
+import { StoreModule } from '@ngrx/store';
+import { effectsArr } from './core/store/effects/index';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +16,12 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot(appReducers),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+    }),
+    EffectsModule.forRoot(effectsArr),
   ],
   providers: [],
   bootstrap: [AppComponent]
